53a65ac517c9d678433819b65dfa142491344f21
CanseFr <jucansell@gmail.com>
2024-11-18 11:30:26 +0100

    :sparkles: Setup login form front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Grid from "@mui/material/Grid2";
+import {Button, TextField} from "@mui/material";
+
+
+interface UserLogin {
+    email: string;
+    password: string;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = React.useState<UserLogin>({ email: "", password: "" });
+
+    const handleSendLogin = (event?: React.FormEvent) => {
+        // Effectuer la requête
+        if (event) event.preventDefault();
+        console.log(user);
+    };
+
+    const handleChange = (field: keyof UserLogin) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUser((prevUser) => ({
+            ...prevUser,
+            [field]: event.target.value,
+        }));
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" width={150}/>
+                <form onSubmit={handleSendLogin}>
+                    <Grid container flexDirection="column" spacing={2} sx={{backgroundColor: "white", borderRadius: "15px", padding: "50px"}}>
+                        <TextField onChange={handleChange("email")} label="Email" variant="outlined"/>
+                        <TextField onChange={handleChange("password")} label="Password" variant="outlined" type="password"/>
+                        <Button type="submit" onClick={handleSendLogin}>Login</Button>
+                    </Grid>
+                </form>
+            </header>
+        </div>
+);
 }
 
 export default App;
+
+
